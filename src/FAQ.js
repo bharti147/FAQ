ccbb8e303b3d1e89cb7472fe008b43793bfc222f
Bharti <sahanibharti71@gmail.com>
2023-07-16 17:58:34 +0530

    2nd

---
+++ src/FAQ.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FAQ extends Component {
+  render() {
+    return (
+      <div>
+        <h2>FAQ</h2>
+      </div>
+    )
+  }
+}
+
+export default FAQ
